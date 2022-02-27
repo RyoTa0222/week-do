fb6ad401a1e1d23621f400d31c8e176b743c322e
Matsumoto Ryota <awpjdmga1943@icloud.com>
2022-02-27 11:38:25 +0900

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: false,
+  darkMode: 'media',
   theme: {
     extend: {},
   },
