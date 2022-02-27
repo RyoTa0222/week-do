10feb6ba9b04c1e08ae68af322b909cbc378f176
Matsumoto Ryota <awpjdmga1943@icloud.com>
2022-02-27 11:37:32 +0900

    add Head component

---
+++ src/components/Head.tsx
@@ -0,0 +1,60 @@
+import Head from 'next/head'
+import { TITLE, DESCRIPTION, KEYWORD, IMAGE } from '@/consts'
+import { OgType } from '@/types'
+
+interface Props {
+  title?: string
+  description?: string
+  image?: string
+  url?: string
+  ogType?: OgType
+}
+
+const HeadComponent: React.FC<Props> = ({
+  title,
+  description,
+  image,
+  url,
+  ogType,
+  children,
+}) => {
+  const _title = title && title.length > 0 ? `${title} – ${TITLE}` : TITLE
+  const _description =
+    description && description.length > 0 ? description : DESCRIPTION
+  const _image = image && image.length > 0 ? image : IMAGE
+  const _url =
+    url && url.length > 0 ? url : `${process.env.NEXT_PUBLIC_SITE_URL}/`
+  const _ogType = ogType && ogType.length > 0 ? ogType : `website`
+  return (
+    <Head>
+      <title>{_title}</title>
+      <meta content={_description} name="description"></meta>
+      <meta property="og:title" content={_title} />
+      <meta property="og:description" content={_description} />
+      <meta name="keywords" content={KEYWORD} />
+      <meta property="og:type" content={_ogType} />
+      <meta property="og:url" content={_url} />
+      <meta property="og:image" content={_image} />
+      <meta property="og:site_name" content={_title} />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@RyoTa___0222" />
+      <meta name="twitter:url" content={_image} />
+      <meta name="twitter:title" content={_title} />
+      <meta name="twitter:description" content={_description} />
+      <meta name="twitter:image" content={_image} />
+      <link rel="canonical" href={_url} />
+      <link
+        rel="shortcut icon"
+        href={`${process.env.NEXT_PUBLIC_SITE_URL}/favicons/favicon.ico`}
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href={`${process.env.NEXT_PUBLIC_SITE_URL}/favicons/apple-touch-icon.png`}
+      />
+      {children}
+    </Head>
+  )
+}
+
+export default HeadComponent
---
+++ src/consts/meta.ts
@@ -0,0 +1,5 @@
+export const TITLE = 'Week Do'
+export const DESCRIPTION = ''
+export const KEYWORD = 'TODO'
+// TODO
+export const IMAGE = `${process.env.NEXT_PUBLIC_SITE_URL}/images/mv.png`
