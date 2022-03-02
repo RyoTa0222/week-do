ade1d78181ac160bf85c24c40e6bfc7c02f0ac9f
Matsumoto Ryota <awpjdmga1943@icloud.com>
2022-03-03 08:46:53 +0900

    topページの仮置き

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,26 @@
 import type { NextPage } from 'next'
+import Button from '@/components/Button'
+// import Logo from '@/components/Logo'
+import { useAuthContext } from '@/contexts/AuthProvider'
 
 const Home: NextPage = () => {
+  const { logout } = useAuthContext()
   return (
-    <div className="flex justify-center items-center flex-col min-h-screen font-mono">
-      Hello Nextjs
+    <div className="flex justify-center items-center flex-col font-mono">
+      {/* <Logo />
+      <Logo type="small" /> */}
+      <Button type="filled" handleClick={logout!}>
+        logout
+      </Button>
+      <Button type="filled" handleClick={() => console.log('aaa')} disabled>
+        disabled
+      </Button>
+      <Button type="outlined" handleClick={() => console.log('aaa')}>
+        action
+      </Button>
+      <Button type="outlined" handleClick={() => console.log('aaa')} processing>
+        processing
+      </Button>
     </div>
   )
 }
